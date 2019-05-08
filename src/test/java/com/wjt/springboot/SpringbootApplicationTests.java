bec9bde0378bd3614b8285f805e5d344b774cf12
wangtao455494922 <455494922@qq.com>
2019-05-08 11:14:36 +0800

    集成redis

--- src/test/java/com/wjt/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/wjt/springboot/SpringbootApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -14,12 +16,26 @@ public class SpringbootApplicationTests
 {
 
     @Autowired
-    SpringbootUserMapper springbootUserMapper;
+    private SpringbootUserMapper springbootUserMapper;
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
 
     @Test
     public void insertUser(){
         SpringbootUser user = new SpringbootUser();
-        user.setName("王金涛");
+        user.setName("wangjintao");
         springbootUserMapper.insert(user);
     }
+
+    @Test
+    public void testRedis() throws Exception {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("name", "wangjintao");
+        String value = ops.get("name");
+        System.out.println(value);
+    }
+
+
 }
